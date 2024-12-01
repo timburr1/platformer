99c171b1baa675593c7a62e75138270d80aead3b
Tim Burr <tim.r.burr@gmail.com>
2024-12-01 11:09:32 -0700

    part 4: double-jump and wall jump

--- player.gd
+++ player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var movement_data : PlayerMovementData
 
+var air_jump = false
+var just_wall_jumped = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
@@ -9,6 +11,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
+	handle_wall_jump()
 	handle_jump()
 	var input_axis := Input.get_axis("ui_left", "ui_right")
 	handle_acceleration(input_axis, delta)
@@ -20,18 +23,35 @@ func _physics_process(delta: float) -> void:
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0.0
 	if just_left_ledge:
 		coyote_jump_timer.start()
+	just_wall_jumped = false
 	
 func apply_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * movement_data.gravity_scale * delta
 
+func handle_wall_jump() -> void:
+	if not is_on_wall_only():
+		return
+	var wall_normal = get_wall_normal()
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.x = wall_normal.x * movement_data.speed
+		velocity.y = movement_data.jump_velocity
+		just_wall_jumped = true
+	
 func handle_jump() -> void:
+	if is_on_floor():
+		air_jump = true
+		
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = movement_data.jump_velocity
 	if not is_on_floor():
 		if Input.is_action_just_released("ui_accept") and velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
+			
+		if Input.is_action_just_pressed("ui_accept") and air_jump and not just_wall_jumped:
+			velocity.y = movement_data.jump_velocity * 0.8
+			air_jump = false
 
 func apply_friction(input_axis:float, delta: float) -> void:
 	if input_axis == 0.0 and is_on_floor():
@@ -42,9 +62,12 @@ func apply_air_resistance(input_axis:float, delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistance * delta)
 	
 func handle_acceleration(input_axis:float, delta: float) -> void:
-	if input_axis != 0.0:
-		velocity.x = move_toward(velocity.x, input_axis * movement_data.speed, movement_data.acceleration * delta)
-
+	if input_axis != 0.0: 
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, input_axis * movement_data.speed, movement_data.acceleration * delta)
+		else:
+			velocity.x = move_toward(velocity.x, input_axis * movement_data.speed, movement_data.air_acceleration * delta)
+	
 func update_animations(input_axis: float) -> void:
 	if input_axis != 0.0:
 		animated_sprite_2d.play("run")
